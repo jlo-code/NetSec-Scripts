34492277d6a099127297aece5bb65ebfce0675a2
J Lo <gabrieljnlouis@gmail.com>
2023-10-30 19:35:34 -0700

    .

---
+++ pwgen.py
@@ -0,0 +1,24 @@
+# Password Encoder
+# Author: j-l0
+# A simple, offline password encoder.
+# The purpose of the script is to assist in circumventing authentication attacks by 
+# creating stronger passwords via encoding.
+
+#Modules
+import base64
+import string
+import pyperclip3
+
+#User input of plaintest password
+print('Tip: Setting a password atleast 12 characters in length is recommended.')
+strpw = input('Please enter your plaintext password: ')
+
+#Encoding plaintext > string > bytes
+bytepw = strpw.encode('utf-8')
+encpw = base64.b64encode(bytepw)
+pyperclip3.copy(encpw)
+
+#Copied to clipboard
+print('Copied to clipboard.')
+
+input('Press enter to close.')
