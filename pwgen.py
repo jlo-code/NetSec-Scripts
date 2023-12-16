f168b1d467ec7c6b7be6dae21befbbac8188aa37
jlo <31631306+jlo-code@users.noreply.github.com>
2023-12-16 07:41:08 -0500

    Update pwgen.py

--- pwgen.py
+++ pwgen.py
@@ -1,26 +1,78 @@
-# Password Encoder
-# Author: j-l0
-# A simple, offline password encoder.
-# The purpose of the script is to assist in circumventing authentication attacks by 
-# creating stronger passwords via encoding.
-
-#Modules
-import subprocess
+import bcrypt 
+import hashlib
 import base64
-import string
-import pyperclip3
+import os
+import sys
+import maskpass
+import pyperclip3 as pc
 
-# #User input of plaintext password
-print('Please temporarily disable Wi-Fi or network connections. \nTip: Using a password atleast 12 characters in length is recommended.')
-strpw = input('Please enter your plaintext password: ')
 
-#Encoding plaintext > string > bytes
-bytepw = strpw.encode('utf-8')
-encpw = base64.b64encode(bytepw)
-pyperclip3.copy(encpw)
+def quit():
+     sys.exit(0)
 
-#Copied to clipboard
-print('Copied to clipboard.')
+def hashed():
+    password = maskpass.askpass(mask="") 
+    bytes = password.encode('utf-8')     # converting password to array of bytes 
+    salt = bcrypt.gensalt()     # generating the salt 
+    hash = bcrypt.hashpw(bytes, salt)     # Hashing the password 
+    pc.copy(hash)
 
-#exit program
-input('Press enter to close.')
+def base64en():
+    password = maskpass.askpass(mask="") 
+    password_bytes = password.encode('utf-8')
+    # Encode the password in Base64
+    encoded_password = base64.b64encode(password_bytes)
+    pc.copy(encoded_password)
+
+def base32en():
+    password = maskpass.askpass(mask="") 
+    password_bytes = password.encode('utf-8')
+    # Encode the password in Base64
+    encoded_password = base64.b32encode(password_bytes)
+    pc.copy(encoded_password)
+
+def base16en():
+    password = maskpass.askpass(mask="") 
+    password_bytes = password.encode('utf-8')
+    # Encode the password in Base64
+    encoded_password = base64.b16encode(password_bytes)
+    pc.copy(encoded_password)
+
+
+
+def menu():
+    print('----------------------------------------------------------')
+    print('                 Python Password Encryption Tool          ')
+    print('----------------------------------------------------------')
+
+    print('1) Select Bcrypt Encryption')
+    print('2) Select Base64 Encoding')
+    print('3) Select Base32 Encoding')
+    print('4) Select Base16 Encoding')
+    print('5) exit')
+
+def main():
+    while True:
+        menu()
+
+        try:
+            choice = int(input("Enter your choice (1-5): "))
+        except ValueError:
+            print("Invalid input. Please enter a number 1 through 5.")
+            continue
+        if choice == 1:
+            hashed()
+        elif choice == 2:
+            base64en()
+        elif choice == 3:
+            base32en()
+        elif choice == 4:
+            base16en()
+        elif choice == 5:
+            quit()
+
+            
+if __name__ == "__main__": 
+	main()
+    
+
